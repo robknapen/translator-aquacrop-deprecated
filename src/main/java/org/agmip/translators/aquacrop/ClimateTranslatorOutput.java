2ef65b3ade58d8cb933cf0198fbc57a43b065882
= <rob.knapen@wur.nl>
2012-07-09 12:23:25 -0400

    Trying local git

---
+++ src/main/java/org/agmip/translators/aquacrop/ClimateTranslatorOutput.java
@@ -0,0 +1,18 @@
+package org.agmip.translators.aquacrop;
+
+import java.util.Map;
+import org.agmip.core.types.TranslatorOutput;
+
+
+/**
+ * AgMIP Output Translator to generate AquaCrop climate input data.
+ * 
+ * @author Rob Knapen <rob.knapen at wur.nl>
+ */
+public class ClimateTranslatorOutput implements TranslatorOutput {
+
+    public void writeFile(String string, Map map) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
